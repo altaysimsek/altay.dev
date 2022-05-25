19428bd036dbe941c6c47d2ff58079737088933d
Altay Simsek <altaysimsek16@gmail.com>
2022-05-25 15:51:06 +0300

    add: dummy sections

---
+++ components/sections/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+    </section>
+  );
+};
+
+export default Projects;
---
+++ components/sections/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1 className="text-white">
+        Welcome to the React-Redux-React-Router-Redux-Firebase boilerplate
+      </h1>
+    </section>
+  );
+};
+
+export default Welcome;
