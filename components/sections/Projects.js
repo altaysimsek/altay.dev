6211d9b42abd3c799960357984a2aa23d58b47c4
Altay Simsek <altaysimsek16@gmail.com>
2022-07-04 04:06:04 +0300

    feat: finished static projects section

--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -2,10 +2,11 @@ import React from "react";
 const Projects = () => {
   return (
     <section>
-      <h2 className="text-white text-4xl font-bold tracking-wider">Projects</h2>
-      <div className="flex flex-wrap">
+      <h2 className="text-white underline decoration-[#F2FE67] underline-offset-2
+ text-4xl font-bold tracking-wider">Projects</h2>
+      <div className="flex flex-wrap justify-center " >
         <div
-          className="w-96 h-96 flex m-4 justify-center items-center flex-col relative"
+          className="w-80 h-80 flex m-4 justify-center items-center flex-col relative"
           style={{ backgroundColor: "#F2FE67" }}
         >
           <div>
@@ -19,7 +20,21 @@ const Projects = () => {
           <p>Kesy is URL shortener.</p>
         </div>
         <div
-          className="w-96 h-96 flex m-4 justify-center items-center flex-col relative"
+          className="w-80 h-80 flex m-4 justify-center items-center flex-col relative"
+          style={{ backgroundColor: "#fff", }}
+        >
+          <div>
+            <img
+              src="/phohipologo.png"
+              alt="Phohipo"
+              width="122px"
+              height="47px"
+            />
+          </div>
+          <p>Phohipo is a Unsplash client.</p>
+        </div>
+        <div
+          className="w-80 h-80 flex m-4 justify-center items-center flex-col relative"
           style={{ backgroundColor: "#220FD5", color: "#F2FE67" }}
         >
           <div>
@@ -32,8 +47,23 @@ const Projects = () => {
           </div>
           <p>Klima is a weather app.</p>
         </div>
+
+        <div
+          className="w-80 h-80 flex m-4 justify-center items-center flex-col relative"
+          style={{ backgroundColor: "#834eff", color: "#fff" }}
+        >
+          <div>
+            <img
+              src="/moviuplogo.png"
+              alt="MovieUP"
+              width="122px"
+              height="47px"
+            />
+          </div>
+          <p>MovieUP is an IMDB client.</p>
+        </div>
       </div>
-    </section>
+    </section >
   );
 };
 
