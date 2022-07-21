85d254c1274deb6710ecd5a23ade531c5a454120
Altay Simsek <altaysimsek16@gmail.com>
2022-07-21 17:58:29 +0300

    chore: meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,12 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta charset="UTF-8" />
+        <meta name="description" content="Hello, It is Altay. You can reach me out there." />
+        <meta name="keywords" content="HTML, CSS, JavaScript, ReactJS" />
+        <meta name="author" content="Altay Simsek" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       </Head>
       <Header />
       <main className="container mx-auto sm:px-16">
