66bbb72df091a64fc1119986bb482a42d27dfeb6
Altay Simsek <altaysimsek16@gmail.com>
2022-10-01 01:41:42 +0300

    feat(fun): new features kind easteregg to my portfolio

---
+++ components/fun/Fun.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="text-white">
+      <video controls>
+        <source src="vfun/ciftetelli.mp4" type="video/mp4" />
+      </video>
+    </div>
+  )
+}
+
+export default Contact
